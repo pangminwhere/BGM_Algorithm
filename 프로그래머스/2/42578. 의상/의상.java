6f060c8150c9b593bd3b8e446dab0da2a4d3bc86
pangminwhere <qoendqh@naver.com>
2024-07-05 13:14:43 +0900

    [level 2] Title: 의상, Time: 0.05 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < clothes.length; i++) {
+            String key = clothes[i][1];
+            map.put(key, map.getOrDefault(key, 0) + 1);
+        }
+        
+        Iterator<Integer> iter = map.values().iterator();
+
+        while (iter.hasNext()) {
+            answer *= iter.next().intValue() + 1;
+        }
+
+        return answer - 1;
+    }
+}
