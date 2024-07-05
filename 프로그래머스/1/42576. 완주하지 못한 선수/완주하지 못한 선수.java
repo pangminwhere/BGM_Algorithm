4247ab60ef3d83bcad2dc8be60d3fb71d53c02d7
pangminwhere <qoendqh@naver.com>
2024-07-05 13:14:20 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 255.35 ms, Memory: 95.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        int i = 0;
+        for (i = 0; i < completion.length; i++) {
+            if (!participant[i].equals(completion[i])) {
+                break;
+            }
+        }
+        answer = participant[i];
+        return answer;
+    }
+}
