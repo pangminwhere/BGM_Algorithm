140bb68bad10980ae160a06b651f43eb1a0c2b2d
pangminwhere <qoendqh@naver.com>
2024-08-01 17:44:55 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.16 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,57 +1,52 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(String[][] places) {
-        int[] answer = new int[places.length];
-        //✅ 각 대기실 별로 거리두기 준수 여부를 확인
-        for (int i = 0; i < answer.length; i++) {
-            if (check(places[i])) answer[i] = 1;
-        }
-        return answer;
-    }
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static boolean[][] visit;
     
-    boolean check(String[] place) {
-        for (int r = 0; r < 5; r++) {
-            for (int c = 0; c < 5; c++) {
-	            	//✅ 응시자(P) 위치 마다 BFS를 수행
-                if (place[r].charAt(c) == 'P') {
-                    if (!bfs(r, c, place)) return false;
-                }
+    static int[] answer;
+    
+    public void dfs(int num, int x, int y, int count, String[] places) {
+        if (count > 2) return;
+        if (count > 0 && count <= 2 && places[x].charAt(y) == 'P') {
+            answer[num] = 0;
+            return;
+        }
+        
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if (nx >= 0 && nx < 5 && ny >= 0 && ny < 5 && places[nx].charAt(ny) != 'X') {
+               if (visit[nx][ny]) {
+                   continue;
+               } 
+                visit[nx][ny] = true;
+                dfs(num, nx, ny, count + 1, places);
+                visit[nx][ny] = false;
             }
         }
-        return true;
     }
     
-    boolean bfs(int r, int c, String[] place) {
-        final int[] dr = {  0,  1,  0, -1 };
-        final int[] dc = {  1,  0, -1,  0 };
-        boolean[][] visited = new boolean[5][5];
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.add(new int[]{ r, c, 0 });
-        visited[r][c] = true;
-
-        while (!queue.isEmpty()) {
-            int[] cur = queue.remove();
-            if (cur[2] >= 2) continue;
-            
-            for (int i = 0; i < 4; i++) {
-                int nr = cur[0] + dr[i];
-                int nc = cur[1] + dc[i];
-                int ndist = cur[2]+1;
-                if (inRange(nr, nc) && place[nr].charAt(nc) != 'X') {
-                    if (!visited[nr][nc]) {
-                        //✅ 거리두기를 준수하지 않았기 때문에 False를 반환한다.
-                        if (place[nr].charAt(nc) == 'P') return false;
-                        queue.add(new int[]{ nr, nc, ndist });
-                        visited[nr][nc] = true;    
+    public int[] solution(String[][] places) {
+        answer = new int[places.length];
+        for (int i = 0; i < places.length; i++) {
+            answer[i] = 1;
+        }
+        
+        for (int i = 0; i < places.length; i++) {
+            visit = new boolean[5][5];
+            for (int j = 0; j < 5; j++) {
+                for (int k = 0; k < 5; k++) {
+                    if (places[i][j].charAt(k) == 'P') {
+                        visit[j][k] = true;
+                        dfs(i, j, k, 0, places[i]);
+                        visit[j][k] = false;
                     }
                 }
             }
         }
-        return true;
-    }
-    
-    boolean inRange(int r, int c) {
-        return (r >= 0) && (r < 5) && (c >= 0) && (c < 5);
+        return answer;
     }
 }
