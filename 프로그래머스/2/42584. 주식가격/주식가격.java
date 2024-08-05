10fc23e10ae57cae88ba7c71c71bb901e551bf65
pangminwhere <qoendqh@naver.com>
2024-08-05 10:23:12 +0900

    [level 2] Title: 주식가격, Time: 32.33 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -4,22 +4,22 @@ class Solution {
     public static int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
         Queue<Integer> q = new LinkedList<>();
-
+        
         for (int i : prices) {
             q.add(i);
         }
-
+        
         int index = 0;
+        
         while (!q.isEmpty()) {
             int currentPrice = q.poll();
-            // currentPrice의 다음 가격부터 반복문을 진행 = i = (prices.length - q.size()
+            
             for (int i = (prices.length - q.size()); i < prices.length; i++) {
-                // 가격이 떨어 졌을 경우
                 if (currentPrice > prices[i]) {
                     answer[index]++;
                     break;
                 }
-                // 가격이 떨어지지 않았을 경우
+                
                 if (currentPrice <= prices[i]) {
                     answer[index]++;
                 }
