973c6527f8acf62d7af9e6fe11a692bcfa21e645
pangminwhere <qoendqh@naver.com>
2024-08-05 19:52:13 +0900

    [level 2] Title: 타겟 넘버, Time: 7.33 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -2,18 +2,18 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] numbers, int target) {
-		    //✅ DFS 탐색을 통해 합이 target이 되는 경우의 수를 구한다.
         return dfs(numbers, 0, target, 0);
     }
     
-    int dfs(int[] numbers, int index, int target, int cur) {
+    int dfs(int[] numbers, int index, int target, int curr) {
         if (index == numbers.length) {
-            return (cur == target) ? 1 : 0;
+            return (curr == target) ? 1 : 0;
         }
         
         int sum = 0;
-        sum += dfs(numbers, index + 1, target, cur + numbers[index]);
-        sum += dfs(numbers, index + 1, target, cur - numbers[index]);
+        
+        sum += dfs(numbers, index + 1, target, curr + numbers[index]);
+        sum += dfs(numbers, index + 1, target, curr - numbers[index]);
         
         return sum;
     }
