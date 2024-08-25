824b04a29e6fb0948f76a1568e883012caa575eb
pangminwhere <qoendqh@naver.com>
2024-08-25 18:27:18 +0900

    [level 2] Title: 소수 찾기, Time: 4.19 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -7,22 +7,19 @@ class Solution {
         permutation(numbers, 0, new boolean[numbers.length()], 0);
         
         int answer = 0;
-        
         for (int num : candidates) {
             if (isPrime(num)) answer++;
         }
-        
         return answer;
     }
     
-    void permutation(String numbers, int current, boolean[] visited, int digit) {
+    void permutation(String numbers, int curr, boolean[] visited, int digit) {
         if (digit == numbers.length()) return;
         
         for (int i = 0; i < numbers.length(); i++) {
             if (visited[i]) continue;
             
-            int newValue = current + (int)((numbers.charAt(i) - '0') * Math.pow(10, digit));
-            
+            int newValue = curr + (int)((numbers.charAt(i) - '0') * Math.pow(10, digit));
             candidates.add(newValue);
             
             visited[i] = true;
