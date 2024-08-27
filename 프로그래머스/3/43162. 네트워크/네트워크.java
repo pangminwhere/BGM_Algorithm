44b58f78d6c2c343aa4eebc06ac6a7bb181ee795
pangminwhere <qoendqh@naver.com>
2024-08-27 16:32:38 +0900

    [level 3] Title: 네트워크, Time: 0.16 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,23 +1,23 @@
 class Solution {
     public int solution(int n, int[][] computers) {
         int count = 0;
-        boolean[] visited = new boolean[n + 1];
+        boolean[] visited = new boolean[n];
         
-        for (int i = 0; i < n; i++) {
-            if(visited[i]) continue;
-            dfs(n, computers, visited, i);
+        for (int i = 0; i < computers.length; i++) {
+            if (visited[i]) continue;
+            dp(n, computers, visited, i);
             count++;
         }
         
         return count;
     }
     
-    void dfs(int n, int[][] computers, boolean[] visited, int curr) {
+    void dp(int n, int[][] computers, boolean[] visited, int curr) {
         visited[curr] = true;
         
         for (int i = 0; i < n; i++) {
             if (!visited[i] && computers[curr][i] == 1) {
-                dfs(n, computers, visited, i);
+                dp(n, computers, visited, i);
             }
         }
     }
