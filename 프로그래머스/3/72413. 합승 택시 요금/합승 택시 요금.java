dc5e021545639a9d9e8db7000d3e1214a22c52eb
pangminwhere <qoendqh@naver.com>
2024-08-28 22:27:10 +0900

    [level 3] Title: 합승 택시 요금, Time: 36.27 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -11,55 +11,52 @@ class Solution {
         }
         
         @Override
-        public int compareTo(Edge next) {
-            return Integer.compare(this.cost, next.cost);
+        public int compareTo(Edge o) {
+            return Integer.compare(this.cost, o.cost);
         }
     }
-    
-    
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        int answer = Integer.MAX_VALUE;
+        int answer= Integer.MAX_VALUE;
         Map<Integer, List<Edge>> graph = new HashMap<>();
         
         for (int i = 1; i <= n; i++) {
-            graph.put(i, new ArrayList());
+            graph.put(i, new ArrayList<>());
         }
         
-        for (int[] next : fares) {
-            graph.get(next[0]).add(new Edge(next[1], next[2]));
-            graph.get(next[1]).add(new Edge(next[0], next[2]));
+        for (int[] fare : fares) {
+            graph.get(fare[0]).add(new Edge(fare[1], fare[2]));
+            graph.get(fare[1]).add(new Edge(fare[0], fare[2]));
         }
         
         int[] answerS = dijkstra(graph, s);
         int[] answerA = dijkstra(graph, a);
         int[] answerB = dijkstra(graph, b);
         
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) { 
             int tmp = answerS[i] + answerA[i] + answerB[i];
-            answer = Math.min(answer, tmp);
+            answer = Math.min(tmp, answer);
         }
         return answer;
     }
     
     int[] dijkstra(Map<Integer, List<Edge>> graph, int start) {
         int n = graph.size();
-        int[] distance = new int[n+1];
+        int[] distance = new int[n + 1];
         Arrays.fill(distance, Integer.MAX_VALUE);
         
-        Queue<Edge> que = new PriorityQueue();
-        que.add(new Edge(start, 0));
+        Queue<Edge> pq = new PriorityQueue<>();
+        pq.add(new Edge(start, 0));
         distance[start] = 0;
         
-        while(!que.isEmpty()) {
-            Edge edge = que.remove();
+        while(!pq.isEmpty()) {
+            Edge curr = pq.remove();
             
-            if (distance[edge.node] < edge.cost) continue;
-            
-            for (Edge next : graph.get(edge.node)) {
-                int nextCost = distance[edge.node] + next.cost;
+            if (distance[curr.node] < curr.cost) continue;
+            for (Edge next : graph.get(curr.node)) {
+                int nextCost = distance[curr.node] + next.cost;
                 if (distance[next.node] > nextCost) {
+                    pq.add(new Edge(next.node, nextCost));
                     distance[next.node] = nextCost;
-                    que.add(new Edge(next.node, nextCost));
                 }
             }
         }
